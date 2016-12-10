bb457975ddf1576c3b955630c89b3aae94441898
fiveinfinity <jlachance1@gmail.com>
2016-12-09 22:12:44 -0600

    site working, tests passing

--- components/App.js
+++ components/App.js
@@ -2,18 +2,49 @@ const React = require('react');
 
 const Filters = require('./Filters');
 const PetBrowser = require('./PetBrowser');
+import * as allPets from '../data/pets'
 
 class App extends React.Component {
   constructor() {
     super();
 
+    this.changeFilters = this.changeFilters.bind(this)
+    this.findPets = this.findPets.bind(this)
+    this.addPet = this.addPet.bind(this)
+
     this.state = {
-      pets: [],
+      pets: [allPets],
       adoptedPets: [],
       filters: {
         type: 'all',
       }
     };
+  }
+
+  changeFilters(type) {
+    this.setState({
+      filters: Object.assign({}, this.state.filters, {
+        type: type
+      })
+    })
+  }
+
+  findPets() {
+    let url = '/api/pets'
+
+    if(this.state.filters.type !== 'all') {
+      url += `?type=${this.state.filters.type}`
+    }
+
+    fetch(url)
+    .then(res => res.json())
+    .then(pets => this.setState({ pets }))
+  }
+
+  addPet(id) {
+    this.setState({
+      adoptedPets: [...this.state.adoptedPets, id]
+    })
   }
 
   render() {
@@ -25,10 +56,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters filters={this.state.filters} onChangeType={this.changeFilters} onFindPetsClick={this.findPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.addPet} adoptedPets={this.state.adoptedPets} />
             </div>
           </div>
         </div>
