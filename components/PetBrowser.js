1ffecabdaf73b6427cbfa8c1c6aa2338b7cf3d13
Justin Thomas <jthomas0209@gmail.com>
2016-12-12 12:26:36 -0500

    Components written, tests passed

--- components/PetBrowser.js
+++ components/PetBrowser.js
@@ -3,11 +3,19 @@ const React = require('react');
 const Pet = require('./Pet');
 
 class PetBrowser extends React.Component {
-  render() {
+	constructor(){
+		super();
+	}
+
+	render() {
+		const pets = this.props.pets.map(pet =>{
+			return <Pet pet = {pet} isAdopted = {this.props.adoptedPets.includes(pet.id)} onAdoptPet = {this.props.onAdoptPet}/>
+		});
+		
     return (
       <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
-      </div>
+      	{pets}
+			</div>
     );
   }
 }
